75462ce199f44e242aa900b3aa6b087abc84085a
NurmuhammadovHojiakbar <pythoncoderuz@gmail.com>
2021-11-12 09:23:44 +0500

    feedbacks length

--- src/Components/BoardHeader/BoardHeader.js
+++ src/Components/BoardHeader/BoardHeader.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import "./BoardHeader.css";
 import Bulb from "../../Assets/Icons/bulb-icon.png"
 import AddFeedback from "../AddFeedback/AddFeedback"
@@ -9,12 +9,20 @@ const BoardHeader = () => {
 
     const {user} = useUser();
     const {feedbackdata} = useFeedback()
+    const [currentdata, setCurrentdata] = useState(null)
+
+    useEffect(()=>{
+        const currentData = feedbackdata.filter(f =>{
+            return f.display === "block"
+        })
+        setCurrentdata(currentData)
+    },[feedbackdata])
 
     return (
         <header className="board-header">
             <h2 className="board-header__suggestion">
                 <img src={Bulb} alt="Bulb" />
-                <span>{ feedbackdata?.length } Suggestions</span>
+                <span>{ currentdata?.length } Suggestions</span>
             </h2>
             {user && <AddFeedback />}
         </header>
