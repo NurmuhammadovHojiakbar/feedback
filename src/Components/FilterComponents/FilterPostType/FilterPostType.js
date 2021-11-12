15496b4ee3b935cd44f566ac4803d15ad9dd77d1
NurmuhammadovHojiakbar <pythoncoderuz@gmail.com>
2021-11-12 13:57:40 +0500

    map changed to forEach

--- src/Components/FilterComponents/FilterPostType/FilterPostType.js
+++ src/Components/FilterComponents/FilterPostType/FilterPostType.js
@@ -14,7 +14,7 @@ const FilterPostType = () => {
         })
         e.target.classList.add("active")
 
-        const newData = feedbackdata?.map(d=>{
+        const newData = feedbackdata?.forEach(d=>{
             if(e.target.textContent === "Barchasi"){
                 return {...d, display: "block"}
             }else{
