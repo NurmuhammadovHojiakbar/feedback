bffd2fc34f009806811702c765550b925ec495f4
NurmuhammadovHojiakbar <pythoncoderuz@gmail.com>
2021-11-12 09:06:31 +0500

    Not Found section added

--- src/Components/FeedbackList/FeedbackList.js
+++ src/Components/FeedbackList/FeedbackList.js
@@ -1,21 +1,39 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import "./Feedback.css"
 import FeedbackItem from './FeedbackItem';
 import { useFeedback } from '../../Contexts/FeedbackContext';
+import NotFound from '../NotFound/NotFound';
 
 const FeedbackList = () => {
 
     const {feedbackdata} = useFeedback()
+    const [notFound, setNotFound] = useState(false)
+    
+
+    useEffect(()=>{
+        if(feedbackdata.find(f=>{
+            return f.display === "block"
+        })){
+            return setNotFound(false)
+        }else{
+            return setNotFound(true)
+        }
+    },[feedbackdata])
 
     return (
         <>
-            <ul className="feedback-list">
-                {
-                    feedbackdata?.map(f=>(
-                        <FeedbackItem feedback={f} />
-                    ))
-                }
-            </ul> 
+            {
+                notFound? 
+                <NotFound />
+                    : 
+                <ul className="feedback-list">
+                    {
+                        feedbackdata?.map(f=>(
+                            <FeedbackItem feedback={f} />
+                        ))
+                    }
+                </ul>
+            }
         </>
     );
 }
---
+++ src/Components/NotFound/NotFound.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import "./NotFound.css"
+import AddFeedback from "../AddFeedback/AddFeedback"
+import {useUser} from "../../Contexts/UserContext"
+import notFoundImage from "../../Assets/Icons/not-found.png"
+
+const NotFound = () => {
+
+    const {user} = useUser();
+
+    return (
+        <div className="not-found-wrapper">
+            <div className="not-found">
+                <img 
+                    className="not-found__img" 
+                    src={notFoundImage} 
+                    alt="Not Found" 
+                    width="130" 
+                    height="140"
+                />
+                <h2 className="not-found__caption">Bu mavzuda fikr-mulohazalar mavjud emas!</h2>
+                <p className="not-found__text">Bu mavzuda fikr-mulohaza mavjud emas, { user && "lekin siz qo'sha olasiz!" } { !user && "agar qo'shishni hohlasangiz iltimos registratsiyadan o'ting!" }</p>
+                { user && <AddFeedback /> }
+            </div>
+        </div>
+    );
+}
+
+export default NotFound;
